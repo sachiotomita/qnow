7b2d4f39246ba9216bdefbb0228d121e46375c61
gba <gba@email.com>
2019-05-27 20:52:20 +0900

    Create Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div>
+    <Header />
+    <div id="app">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-view/>
     </div>
-    <router-view/>
+    
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Header from '@/components/Header.vue';
+
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class App extends Vue {}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html, body {
+  margin: 0;
+  padding: 0;
 }
-#nav {
+#app {
+  width: 40%;
+  margin: 0 auto;
+}
+/*#nav {
   padding: 30px;
   a {
     font-weight: bold;
@@ -25,5 +40,5 @@
       color: #42b983;
     }
   }
-}
+}*/
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <nav>
+      <div class="site-logo">Qnow</div>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  height: 60px;
+  background-color: #fff;
+  border-bottom: 1px solid #eee;
+  box-shadow: 2px 2px 3px #ccc;
+  nav {
+    width: 800px;
+    margin: 0 auto;
+    .site-logo {
+      font-size: 25px;
+      color: #2196F3;
+
+    }
+  }
+}
+</style>
