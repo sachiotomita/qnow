d82d9d6c5f84880457b12743f084462a464e3a26
gba <gba@email.com>
2019-05-27 21:26:15 +0900

    Create Main Component and Routing Main Component

---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <Search />
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Search from '@/components/main/Search.vue';
+
+@Component({
+  components: {
+    Search,
+  },
+})
+export default class Main extends Vue {
+
+}
+</script>
+
