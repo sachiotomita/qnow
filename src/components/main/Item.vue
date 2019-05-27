1d5c1bb959690d9eb1738e79ec37998a8ef8a1ea
gba <gba@email.com>
2019-05-27 23:27:25 +0900

    Create Item Component

---
+++ src/components/main/Item.vue
@@ -0,0 +1,14 @@
+<template>
+  <li class="item">
+    <div class="item__title"></div>
+  </li>
+</template>
+
+<style lang="scss">
+.item {
+  width: 100%;
+  padding: 20px;
+  border: 1px solid #eee;
+  box-shadow: 2px 2px 2px #ccc;
+}
+</style>
