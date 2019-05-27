b247c9b02e713fcbacd218b169c50a1fc4cd251c
gba <gba@email.com>
2019-05-27 21:24:51 +0900

    Add Fontawesome modules

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faSearch);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
