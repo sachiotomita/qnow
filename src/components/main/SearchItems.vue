fba9b3cbca2a0cb6f05a1cd65335746c50cd8a2e
gba <gba@email.com>
2019-05-28 22:21:23 +0900

    Renamed Search Component to SearchItems Component

---
+++ src/components/main/SearchItems.vue
@@ -0,0 +1,42 @@
+<template>
+  <form v-on:submit.prevent="$emit('set', tagname)">
+    <input id="search-form" type="text" v-model="tagname" /><button type="submit"><font-awesome-icon icon="search" /></button>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SearchItems extends Vue {
+  tagname: string = ""
+}
+</script>
+
+<style lang="scss" scoped>
+form {
+  width: 100%;
+  margin-bottom: 40px;
+  #search-form {
+    border: 0;
+    width: 90%;
+    height: 40px;
+    padding: 15px;
+    font-size: 1.2em;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    &:focus {
+      outline: none;
+    }
+  }
+  button {
+    width: 10%;
+    height: 40px;
+    padding: 10px;
+    border: none;
+    background-color: #2196F3;
+    color: #fff;
+    box-sizing: border-box;
+  }
+}
+</style>
