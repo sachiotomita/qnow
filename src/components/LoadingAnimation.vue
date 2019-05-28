18c4a9520452d52d84a1b0f5ba86802de7f32cff
gba <gba@email.com>
2019-05-29 00:30:15 +0900

    Create LoadingAnimation Component

---
+++ src/components/LoadingAnimation.vue
@@ -0,0 +1,30 @@
+<template>
+<div id="loading"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+export default class LoadingAnimation extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+#loading {
+  width: 60px;
+  height: 60px;
+  margin: 0 auto;
+  border-top: 6px solid #2196F3;
+  border-right: 6px solid #2196F3;
+  border-bottom: 6px solid #2196F3;
+  border-left: 6px solid transparent;
+  border-radius: 50%;
+  animation: loading 1s linear infinite;
+}
+@keyframes loading {
+  0% {}
+  50% { opacity: 0.6; }
+  100% {
+    transform: rotate(360deg);
+    opacity: 0.3;
+  }
+}
+</style>
