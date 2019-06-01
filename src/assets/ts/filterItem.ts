ab6fc7c7ee71c50fa8cda84e9274e48e916be755
gaba <gaba@email.com>
2019-06-01 14:30:04 +0900

    Update SearchItem function

--- src/assets/ts/filterItem.ts
+++ src/assets/ts/filterItem.ts
@@ -6,7 +6,7 @@ const month: string =  ('00' + (date.getMonth() + 1)).slice(-2);
 const day: string = ('00' + (date.getDate())).slice(-2);
 const hour: string = ('00' + (date.getHours())).slice(-2);
 const minutes: string = ('00' + (date.getMinutes())).slice(-2);
-const today: string = `${year}-${month}-${day}-${hour}-${minutes}`;
+const today: string = `${year}-${month}-${day}T${hour}:${minutes}`;
 
 const beforeDate: Date = new Date();
 beforeDate.setDate(beforeDate.getDate() - 1);
@@ -15,12 +15,16 @@ const beforeMonth: string =  ('00' + (beforeDate.getMonth() + 1)).slice(-2);
 const beforeDay: string = ('00' + (beforeDate.getDate())).slice(-2);
 const beforeHour: string = ('00' + (beforeDate.getHours())).slice(-2);
 const beforeMinutes: string = ('00' + (beforeDate.getMinutes())).slice(-2);
-const yesterday: string = `${beforeYear}-${beforeMonth}-${beforeDay}-${beforeHour}-${beforeMinutes}`;
+const yesterday: string = `${beforeYear}-${beforeMonth}-${beforeDay}T${beforeHour}:${beforeMinutes}`;
 
 const filterTag = (items: any): any => {
+  /*const fulldayFilter = (item: { [key: string]: any }): any => {
+    if (item.created_at === today)
+  }*/
   const filterItems: any = items.filter((item: {[ key: string ]: any}): any => {
     console.log(`${today}:${yesterday}`);
-    item.created_at = item.created_at.slice(0, 10);
+    item.created_at = item.created_at.slice(0, 16);
+    console.log(item.created_at)
     return (item.created_at === today);
   });
   return sortLikesCount(filterItems);
